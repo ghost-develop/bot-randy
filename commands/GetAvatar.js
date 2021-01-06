86704e478e3e10382345536111ccd78d8a77b047
Casey Calkins <a.caseycalkins@outlook.com>
2021-01-05 21:01:07 -0600

    Created dynamic command calls

--- commands/GetAvatar.js
+++ commands/GetAvatar.js
@@ -9,6 +9,7 @@ module.exports = {
 		const avatarList = message.mentions.users.map(user => {
 			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
 		});
+		message.channel.send(avatarList);
 	},
 };
 
--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
-client.commands = new Discord.Collections();
+client.commands = new Discord.Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	client.commands.set(command.name, command);
-}
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -23,58 +23,15 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 client.login(token);
-
-// if (command === 'ping') {
-// 	message.channel.send('Pong');
-// }
-// else if (command === 'args-info') {
-// 	if (!args.length) {
-// 		return message.channel.send(`${message.author}, you didn't provide any agruments.`);
-// 	}
-// 	else if (args[0] === 'foo') {
-// 		return message.channel.send('bar');
-// 	}
-// 	message.channel.send(`First Arg: ${args[0]}`);
-// }
-// else if (command === 'kick') {
-// Grab the 'first' mentioned user from the message
-// this will return a 'User' object, just like 'message.author'
-// 	if (!message.mentions.users.size) {
-// 		return message.reply('You need to tag a user in order to kick them');
-// 	}
-// 	const taggedUser = message.mentions.users.first();
-// 	message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-// }
-
-// else if (command === 'message') {
-// 	if (!message.mentions.users.size) {
-// 		return message.reply('You have to tag a user to ping them');
-// 	}
-// 	const taggedUser = message.mentions.users.first();
-// 	taggedUser.send(typeof args);
-// }
-
-// else if (command === 'avatar') {
-// 	if (!message.mentions.users.size) {
-// 		return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
-// 	}
-// 	const avatarList = message.mentions.users.map(user => {
-// 		return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
-// 	});
-// 	message.channel.send(avatarList);
-// }
-// else if (command === 'remove') {
-// 	const amount = parseInt(args[0] + 1);
-
-// 	if (isNaN(amount)) {
-// 		return message.reply('that doesn\'t seem to be a valid number.');
-// 	}
-// 	else if (amount <= 1 || amount > 100) {
-// 		return message.reply('an input between 1 and 100 is required.');
-// 	}
-// 	message.channel.bulkDelete(amount);
-// }
-
